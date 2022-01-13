d40b827847b40b1c5018a8259a3f77a78fe160e2
falconskydiving <falcon.skydiving.dev@gmail.com>
2022-01-13 15:20:00 +0200

    Implement the pagination function

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,42 +1,6 @@
 export const resolvers = {
   Query: {
     events: (_parent, _args, ctx) => {
-
-      // return [
-      //   { 
-      //     name: 'Grundkursus København 01', 
-      //     // teacher: {
-      //     //   name: 'Allan B. Rix', 
-      //     // },
-      //     spotCnt: 17, 
-      //     startDate: '11-10-2021' 
-      //   },
-      //   { 
-      //     name: 'Grundkursus København 02', 
-      //     // teacher: {
-      //     //   name: 'Allan B. Rix', 
-      //     // },
-      //     spotCnt: 17, 
-      //     startDate: '11-10-2021' 
-      //   },
-      //   { 
-      //     name: 'Grundkursus København 03',
-      //     // teacher: {
-      //     //   name: 'Allan B. Rix', 
-      //     // },
-      //     spotCnt: 17, 
-      //     startDate: '11-10-2021'
-      //   },
-      //   { 
-      //     name: 'Grundkursus København 04', 
-      //     // teacher: {
-      //     //   name: 'Allan B. Rix', 
-      //     // },
-      //     spotCnt: 17, 
-      //     startDate: '11-10-2021' 
-      //   },
-      // ]
-
       return ctx.prisma.event.findMany()
     },
   },
